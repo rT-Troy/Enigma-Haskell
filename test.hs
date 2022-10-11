8c653c4cc7059dd980b9b350b151395df404e79e
rT_Troy <yotroy@foxmail.com>
2022-10-11 01:30:31 +0100

    1

--- test.hs
+++ test.hs
@@ -29,9 +29,9 @@ module Test where
   boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
 
 
-  let xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]  
-  [ [ x | x <- xs, even x ] | xs <- xxs]  -- Nested list comprehensions
-  [[2,2,4],[2,4,6,8],[2,4,2,6,2,6]] 
+  --let xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]  
+  --[ [ x | x <- xs, even x ] | xs <- xxs]  -- Nested list comprehensions
+  --[[2,2,4],[2,4,6,8],[2,4,2,6,2,6]] 
 
   -- fst snd
   {- zip  -- can zip finite lists with infinite lists
