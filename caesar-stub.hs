93587e6270ccffadd1b9b0da62fa8bf988e851e5
troy <yotroy@foxmail.com>
2022-10-13 00:21:59 +0100

    optimise folder dir

--- caesar-stub.hs
+++ caesar-stub.hs
@@ -9,11 +9,3 @@ int2let n = chr (ord 'a' + n)
 shift :: Int -> Char -> Char
 shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
           | otherwise = c
-
---exercise 2 q1.1
-encode :: [Char] -> [Int]
-encode xs = [let2int x | x<- xs]
-
---exercise 2 q1.2
-decode :: [Int] -> [Char]
-decode xs = [int2let x | x<- xs]
--- exercise2.hs
+++ exercise2.hs
@@ -1,5 +1,19 @@
 
-import Data.Char
 
-encode :: Char -> Int
-encode c = ord c - ord 'a'
+module Exercise2 where
+
+  import Data.Char
+  
+  let2int :: Char -> Int
+  let2int c = ord c - (ord 'a')
+
+  int2let :: Int -> Char
+  int2let n = chr (ord 'a' + n)
+
+  --exercise 2 q1.1
+  encode :: [Char] -> [Int]
+  encode xs = [let2int x | x<- xs]
+
+--exercise 2 q1.2
+  decode :: [Int] -> [Char]
+  decode xs = [int2let x | x<- xs]
