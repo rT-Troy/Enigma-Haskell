f43ff697c766e4f06daf35c04ebd3c9f0eba90fe
troy <yotroy@foxmail.com>
2022-10-13 12:51:35 +0100

    exercise2 to q5

--- exercise2.hs
+++ exercise2.hs
@@ -10,10 +10,26 @@ module Exercise2 where
   int2let :: Int -> Char
   int2let n = chr (ord 'a' + n)
 
-  --exercise 2 q1.1
+  --q1
   encode :: [Char] -> [Int]
   encode xs = [let2int x | x<- xs]
 
---exercise 2 q1.2
+  --q2
   decode :: [Int] -> [Char]
   decode xs = [int2let x | x<- xs]
+
+  --q3
+  fibonacci 0 = 1
+  fibonacci 1 = 1
+  fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+
+  --q4
+  find' n xs = show (xs !! n)
+
+  --q5
+  roots :: (Float, Float, Float) -> (Float, Float)
+  --roots a b c = show (zip [(-b+sqrt (b^2-4*a*c))/2a] [(-b-sqrt (b^2-4*a*c))/2a])
+  roots (a,b,c) = (x1,x2)
+    where x1 = (-b+sqrt (b^2-4*a*c))/2*a
+          x2 = (-b-sqrt (b^2-4*a*c))/2*a
