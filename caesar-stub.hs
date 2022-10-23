39ab091d2b07f9bd2a8d57742639f5c4c46cff33
rT_Troy <yotroy@foxmail.com>
2022-10-23 03:29:08 +0100

    with week 2 exercise job

--- caesar-stub.hs
+++ caesar-stub.hs
@@ -9,3 +9,15 @@ int2let n = chr (ord 'a' + n)
 shift :: Int -> Char -> Char
 shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
           | otherwise = c
+
+encode :: Int -> String -> String
+encode n input = [ shift n x | x <- input ]
+encode' n input = map (shift n) input
+encode'' n [] = []
+encode'' n (x:xs) = shift n x : encode'' n xs
+
+decode :: Int -> String -> String
+decode n input = [ shift (-n) x | x <- input ]
+decode' n input = map (shift (-n)) input
+decode'' n [] = []
+decode'' n (x:xs) = shift (-n) x : decode'' n xs
--- exercise2.hs
+++ exercise2.hs
@@ -25,16 +25,27 @@ module Exercise2 where
 
 
   --q4
-  find' n xs = show (xs !! n)
+  find' n xs = show (xs !! n) 
+   --show will converts to a String, so could be better like below
+
+  nth 0 (x:xs) = x
+  nth n (x:xs) = nth (n-1) xs
 
   --q5
   roots :: (Float, Float, Float) -> (Float, Float)
-  --roots a b c = show (zip [(-b+sqrt (b^2-4*a*c))/2a] [(-b-sqrt (b^2-4*a*c))/2a])
   roots (a,b,c) = (x1,x2)
     where x1 = (-b+sqrt (b^2-4*a*c))/2*a
           x2 = (-b-sqrt (b^2-4*a*c))/2*a
+  --could be more clear like below
+
+  rootsB (a, b, c) = ((-b+s)/d,(-b-s)/d)
+    where s = sqrt (b*b -4.0*a*c)
+          d = 2.0*a
+
+
   --q7
   rewrite = map (2*) (filter even [1..20])
+   --good
 
   --q8
 
@@ -43,12 +54,29 @@ module Exercise2 where
   --ghci> (\n -> n-1) 5
   --ghci> (\n -> [n%x == 0| x<- [2..n]])   --not work
 
-
+{-
+(Harder!) Check if a value is a prime number
+OK… start with a lambda expression to check if a number n is divisible by another, a:
+(\ n a -> n `mod` a == 0)
+Now generate all the factors of a number p (except 1 and p itself):
+(\ p -> filter (\ a -> p `mod` a == 0) [2..p `div` 2])
+The number is prime if that list is empty:
+(\ x -> null (
+ (\ p -> filter (\ a -> p `mod` a == 0) [2..p `div` 2]) x))
+Still not quite right! We only want numbers > 1:
+(\ x -> x > 1 && null (
+ (\ p -> filter (\ a -> p `mod` a == 0) [2..p `div` 2]) x))
+-}
   --q10.1  
   luhnDouble :: Int -> Int
   luhnDouble n 
     | 2*n > 9 = 2*n-9
     | otherwise = 2*n
+
+  luhnDouble2 n
+    | x > 9 = x - 9
+    | otherwise = x
+    where x = 2*n
   
   --q10.2 --don't really understand the meaning of part 2
 
@@ -73,14 +101,29 @@ module Exercise2 where
   --p a
   sum' [] = 0
   sum' (x:xs) = x + (sum' xs)
+    --good 
   --reminder!!: when do the recursion, it's important to make sure non-exhaustive patterns happended like remember [] case.
   --p b  --s:start --h:how much
   
   take' xs s h = take h (drop s xs)
 
+  take2 :: Int -> [a] -> [a]
+  take2 0 _ = []
+  take2 n (x:xs) = x : take2 (n-1) xs
+
   --p c
   
-  last' xs = 
+  --last' xs = 
+
+  last' :: [a] -> a
+  last' [] = error "Insufficient items in input list"
+  last' [x] = x
+  last' (x:xs) = last' xs
 
 
-  
+  --not finished 
+  --q14
+  zip' :: [a] -> [b] -> [(a,b)]
+  zip' [] _ = []
+  zip' _ [] = []
+  zip' (a:as) (b:bs) = (a,b) : zip' as bs
