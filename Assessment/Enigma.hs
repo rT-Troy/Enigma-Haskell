dab86e908cb0b6ac6debd624d6ea69311debb799
rT-Troy <yotroy@foxmail.com>
2022-11-08 00:02:34 +0000

    add assessment

---
+++ Assessment/Enigma.hs
@@ -0,0 +1,79 @@
+{-- Stub for the grading assignment. Fill it in, making sure you use good
+ -- functional style, and add comments (including replacing those that are
+ -- already here).
+--}
+
+module Enigma where
+  import Data.Char  -- to use functions on characters
+  import Data.Maybe -- breakEnigma uses Maybe type
+  -- add extra imports if needed, but only standard library functions!
+
+{- Part 1: Simulation of the Enigma -}
+
+  type Rotor = Bool -- the supplied type is not correct; fix it!
+  type Reflector = Bool -- the supplied type is not correct; fix it!
+  type Offsets = Bool -- the supplied type is not correct; fix it!
+  type Stecker = Bool -- the supplied type is not correct; fix it!
+  
+  data Enigma = SimpleEnigma Rotor Rotor Rotor Reflector Offsets
+                | SteckeredEnigma Rotor Rotor Rotor Reflector Offsets Stecker
+
+  encodeMessage :: String -> Enigma -> String
+  encodeMessage _ _ = "" -- you need to complete this!
+
+  {- You will need to add many more functions. Remember, design it carefully
+   - and keep it simple! If things are feeling complicated, step back from your
+   - code and think about the design again.
+   -}
+
+
+{- Part 2: Finding the Longest Menu -}
+
+  type Menu = Bool -- the supplied type is not correct; fix it!
+  type Crib = Bool -- the supplied type is not correct; fix it!
+
+  longestMenu :: Crib -> Menu
+  longestMenu _ = False
+
+{- Part 3: Simulating the Bombe -}
+  
+  breakEnigma :: Crib -> Maybe (Offsets, Stecker)
+  breakEnigma _ = Nothing
+
+{- Useful definitions and functions -}
+
+   -- substitution cyphers for the Enigma rotors
+   -- as pairs of (wirings, knock-on position)
+   -- knock-on position is where it will cause the next left wheel to
+   -- advance when it moves past this position
+ 
+        --"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  rotor1=("EKMFLGDQVZNTOWYHXUSPAIBRCJ",17::Int)
+  rotor2=("AJDKSIRUXBLHWTMCQGZNPYFVOE",5::Int)
+  rotor3=("BDFHJLCPRTXVZNYEIWGAKMUSQO",22::Int)
+  rotor4=("ESOVPZJAYQUIRHXLNFTGKDCMWB",10::Int)
+  rotor5=("VZBRGITYUPSDNHLXAWMJQOFECK",0::Int)
+
+  {- the standard Enigma reflector (Reflector B)
+    swapped A<->Y, B<->R, C<->U,D<->H, E<->Q, F<->S, G<->L, 
+            I<->P, J<->X, K<->N, M<->O, T<->Z,V<->W
+  -}
+  reflectorB= [('A','Y'),
+              ('B','R'),
+              ('C','U'),
+              ('D','H'),
+              ('E','Q'),
+              ('F','S'),
+              ('G','L'),
+              ('I','P'),
+              ('J','X'),
+              ('K','N'),
+              ('M','O'),
+              ('T','Z'),
+              ('V','W')]
+
+  {- alphaPos: given an uppercase letter, returns its index in the alphabet
+     ('A' = position 0; 'Z' = position 25)
+   -}
+  alphaPos :: Char -> Int
+  alphaPos c = (ord c) - ord 'A'
