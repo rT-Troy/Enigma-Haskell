f52a1786e903146a1ffa6689b6583367a3e491ee
rT-Troy <yotroy@foxmail.com>
2022-11-13 17:00:47 +0000

    get encode work in front of reflector

--- Assessment/Enigma.hs
+++ Assessment/Enigma.hs
@@ -19,7 +19,15 @@ module Enigma where
                 | SteckeredEnigma Rotor Rotor Rotor Reflector Offsets Stecker
 
   encodeMessage :: String -> Enigma -> String
-  encodeMessage xs (SimpleEnigma rotor1 rotor2 rotor3 reflectorB (0,0,25)) = encodeStr (encodeStr (encodeStr xs rotor1) rotor2) rotor3 -- you need to complete this!
+  encodeMessage [] _ = []
+  encodeMessage (x:xs) (SimpleEnigma rotorR rotorM rotorL reflectorB offsets) = 
+    [encode (encode (encode x rotorR) rotorM) rotorL] ++ encodeMessage xs (SimpleEnigma (head nextRotorList) (head (tail nextRotorList)) (last nextRotorList) reflectorB nextOffsets)
+      where nextOffsets = moveMatch (moveStep (if offsets /= (0,0,25) then offsets else moveStep offsets)) rotorR rotorM rotorL
+            nextRotorList = cur3Rotor offsets nextOffsets rotorR rotorM rotorL
+  -- > encodeMessage "ABC" (SimpleEnigma rotor1 rotor2 rotor3 reflectorB (0,0,25))
+  -- > encodeMessage "AAAAAAAAAA" (SimpleEnigma rotor1 rotor2 rotor3 reflectorB (0,0,25))
+  -- > "AAAAAAA" -> "GVURPWX"
+
 
   {- You will need to add many more functions. Remember, design it carefully
    - and keep it simple! If things are feeling complicated, step back from your
@@ -47,19 +55,38 @@ module Enigma where
   -- > offset ("EKMFLGDQVZNTOWYHXUSPAIBRCJ",17::Int)
   -- > offset (offset ("EKMFLGDQVZNTOWYHXUSPAIBRCJ",17::Int))
 
-  moveStep :: [Int] -> [Int]
-  moveStep [x,y,z] = map (`rem` 26) [x,y,z+1]
-  -- > moveStep [0,25,25]
-  -- > moveStep [0,24,25]
+  moveStep :: (Int,Int,Int) -> (Int,Int,Int)
+  moveStep (x,y,z) = (x `rem` 26,y `rem` 26,(z+1)`rem` 26)
+  -- > moveStep (0,25,25)
+  -- > moveStep (0,24,25)
 
   {- offsets of next step according to knock-on position -}
-  moveMatch :: [Int] -> Rotor -> Rotor -> Rotor -> [Int]
-  moveMatch [x,y,z] rotorL rotorM rotorR
-    | y == rotorInt rotorM && rotorInt rotorR == (z-1) = [x+1,y+1,z]
-    | rotorInt rotorR == (z-1) = [x,y+1,z]
-    | otherwise = [x,y,z]
-  -- > moveMatch (moveStep [0,0,17]) rotor3 rotor2 rotor1
-  -- > moveMatch (moveStep [0,5,17]) rotor3 rotor2 rotor1
+  moveMatch :: (Int,Int,Int) -> Rotor -> Rotor -> Rotor -> (Int,Int,Int)
+  moveMatch (x,y,z) rotorR rotorM rotorL
+    | y == rotorInt rotorM && rotorInt rotorR == (z-1) = (x+1,y+1,z)
+    | rotorInt rotorR == (z-1) = (x,y+1,z)
+    | otherwise = (x,y,z)
+  -- > moveMatch (moveStep (0,0,25)) rotor3 rotor2 rotor1
+  -- > moveMatch (moveStep (0,0,17)) rotor3 rotor2 rotor1
+  -- > moveMatch (moveStep (0,5,17)) rotor3 rotor2 rotor1
+  -- > moveMatch (moveStep (if (0,0,25) == (0,0,25) then moveStep (0,0,25) else (0,0,25))) rotor3 rotor2 rotor1
+  -- > moveMatch (moveStep (if (0,0,1) == (0,0,25) then moveStep (0,0,1) else (0,0,1))) rotor3 rotor2 rotor1
+
+  --moveRotor :: [Int] -> Rotor -> Rotor -> Rotor -> Rotor -> Rotor -> Rotor
+  --moveRotor [x,y,z] rotorL rotorM rotorR = rotorL
+
+
+  {- set current state of rotor by every Char insert -}
+  curRotor :: Int -> Rotor -> Rotor
+  curRotor int rotor = (drop int (rotorStr rotor) ++ (take int (rotorStr rotor)), rotorInt rotor)
+  -- > curRotor 3 rotor1
+
+  cur3Rotor :: (Int,Int,Int) -> (Int,Int,Int) -> Rotor -> Rotor -> Rotor -> [Rotor]
+  cur3Rotor (ix,iy,iz) (x,y,z) rotorR rotorM rotorL =[(drop (z-iz`rem`25) (rotorStr rotorR) ++ (take (z-iz`rem`25) (rotorStr rotorR)), rotorInt rotorR),
+                                                      (drop (y-iy`rem`25) (rotorStr rotorM) ++ (take (y-iy`rem`25) (rotorStr rotorM)), rotorInt rotorM),
+                                                      (drop (x-ix`rem`25) (rotorStr rotorL) ++ (take (x-ix`rem`25) (rotorStr rotorL)), rotorInt rotorL)]
+  -- > cur3Rotor (2,1,25) (2,1,0) rotor1 rotor2 rotor3
+  -- > cur3Rotor (2,1,0) (2,1,1) rotor1 rotor2 rotor3
 
 
   reflector :: Char -> [(Char,Char)] -> Char
@@ -70,14 +97,7 @@ module Enigma where
     | otherwise = reflector c xs
   -- > reflector 'T' reflectorB
 
-  {- set initial state of rotor -}
-  initRotor :: Rotor -> Rotor
-  initRotor rotor = ((drop (rotorInt rotor) (rotorStr rotor)) ++ (take (rotorInt rotor) (rotorStr rotor)),snd(rotor))
-  -- > startRotor ("EKMFLGDQVZNTOWYHXUSPAIBRCJ",17::Int)
 
-  isDividable :: Int -> Bool
-  isDividable n = (n `div` 26) >= 1
-  
   {- encode char to char  by a rotor -}
   encode :: Char -> Rotor -> Char
   encode c rotor = head(drop m (rotorStr rotor))
@@ -129,6 +149,8 @@ module Enigma where
   rotor3=("BDFHJLCPRTXVZNYEIWGAKMUSQO",22::Int)
   rotor4=("ESOVPZJAYQUIRHXLNFTGKDCMWB",10::Int)
   rotor5=("VZBRGITYUPSDNHLXAWMJQOFECK",0::Int)
+
+  -- > "AAAAAAA" -> "GVURPWX"
 
   {- the standard Enigma reflector (Reflector B)
     swapped A<->Y, B<->R, C<->U,D<->H, E<->Q, F<->S, G<->L, 
